6ff7316e2f37e0bbb6429e04b4a19f12727637fa
aditya negara <aditnegara51@gmail.com>
2020-06-29 22:33:14 +0700

    menu managemenet part 1 finish

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -11,34 +11,54 @@
 
 <!-- Divider -->
 <hr class="sidebar-divider ">
+<?php 
+$role_id = $this->session->userdata('role_id');
+  $queryMenu = "SELECT `user_menu` .  `id`, `menu`
+                FROM   `user_menu`JOIN `user_access_menu`
+                ON `user_menu` . `id` = `user_access_menu` . `menu_id`
+                WHERE `user_access_menu` . `role_id` =  $role_id
+                ORDER BY `user_access_menu` . `menu_id` ASC
+                ";
+  $menu = $this->db->query($queryMenu)->result_array();
+ 
 
+
+
+
+?>
+<!-- Looping Menu -->
+<?php foreach($menu as $menu) :  ?>
 <div class="sidebar-heading">
-  Admin
+  <?= $menu['menu'] ?>
 </div>
-<!-- Nav Item - Dashboard -->
-<li class="nav-item">
-  <a class="nav-link" href="index.html">
-    <i class="fas fa-fw fa-tachometer-alt"></i>
-    <span>Dashboard</span></a>
-</li>
+
+<?php
+$menuId = $menu['id'];
+  $querySubMenu = "SELECT * FROM `user_sub_menu` 
+                  Join `user_menu`
+                  ON `user_sub_menu` . `menu_id` = `user_menu` . `id`
+                  WHERE `user_sub_menu` . `menu_id` =  $menuId 
+                  AND `user_sub_menu` .  `is_active` = 1";
+  $subMenu = $this->db->query($querySubMenu)->result_array();
+?>
+  <?php foreach($subMenu as $subMenu) : ?>
+    <li class="nav-item">
+      <a class="nav-link" href="<?= base_url($subMenu['url']);?>">
+        <i class="<?= $subMenu['icon']; ?>"></i>
+        <span><?= $subMenu['title']?></span></a>
+    </li>
+  <?php endforeach; ?>
+  <hr class="sidebar-divider">
+<?php endforeach; ?>
+
+
 
 <!-- Divider -->
-<hr class="sidebar-divider">
-
-<!-- Heading -->
-<div class="sidebar-heading">
-  User
-</div>
-
-<li class="nav-item">
-  <a class="nav-link" href="charts.html">
-  <i class="fas fa-fw fa-user"></i>
-    <span>My Profile</span></a>
-</li>
 
 
-<!-- Divider -->
-<hr class="sidebar-divider">
+
+
+
 
 <li class="nav-item">
   <a class="nav-link" href="<?= base_url('auth/logout')?>">
